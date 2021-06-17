435ec91ac2ec4d9372ec3127ca5520650fe2cb2c
Eleven-Twelv-e <nicat.maharramov.2020@gmail.com>
2021-06-17 08:50:26 +0400

    second try

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,24 +3,27 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String message;
-        Socket socket = new Socket("localhost", 7074);
+       try( Socket socket = new Socket("localhost", 7074);
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in)
+       ) {
+           System.out.println(in.readLine());
+           message = scanner.nextLine();
+           out.println(message);
+           System.out.println(in.readLine());
+           message = scanner.nextLine();
+           out.println(message);
 
-        System.out.println(in.readLine());
-        message = scanner.nextLine();
-        out.println(message);
-        System.out.println(in.readLine());
-        message = scanner.nextLine();
-        out.println(message);
-
-        while (socket.isConnected()) {
-            System.out.println(in.readLine());
-            message = scanner.nextLine();
-            out.println(message);
-        }
+           while (socket.isConnected()) {
+               System.out.println(in.readLine());
+               message = scanner.nextLine();
+               out.println(message);
+           }
+       }catch (IOException e){
+           e.printStackTrace();
+       }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,21 @@ import java.net.Socket;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(7074);
-        Socket clientSocket = serverSocket.accept();
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    public static void main(String[] args)  {
 
-        System.out.println("New connection accepted");
+        try (ServerSocket serverSocket = new ServerSocket(7074);
+             Socket clientSocket = serverSocket.accept();
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ) {
+            System.out.println("New connection accepted");
 
-        final String name = in.readLine();
+            final String name = in.readLine();
 
-        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-
+            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
